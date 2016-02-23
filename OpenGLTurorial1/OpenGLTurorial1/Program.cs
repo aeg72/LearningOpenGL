fb8ef22ff97228a2c5c8efa797ba0b074519c057
aeg72 <aeg72@cornell.edu>
2016-02-23 11:35:47 -0500

    End of Tutorial # 2
    https://www.youtube.com/watch?v=xNGmQ6lO6NY

--- OpenGLTurorial1/OpenGLTurorial1/Program.cs
+++ OpenGLTurorial1/OpenGLTurorial1/Program.cs
@@ -7,6 +7,9 @@ namespace OpenGLTurorial1
     class Program
     {
         private static int width = 1280, height = 720;
+        private static ShaderProgram program;
+        private static VBO<Vector3> triangle, square;
+        private static VBO<int> triangleElements, squareElements;
 
         static void Main(string[] args)
         {
@@ -17,6 +20,17 @@ namespace OpenGLTurorial1
 
             Glut.glutIdleFunc(OnRenderFrame);
             Glut.glutDisplayFunc(OnDisplay);
+
+            program = new ShaderProgram(VertexShader, FragmentShader);
+            program.Use();
+            program["projection_matrix"].SetValue(Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)width / height, 0.1f, 1000f));
+            program["view_matrix"].SetValue(Matrix4.LookAt(new Vector3(0, 0, 10), Vector3.Zero, Vector3.Up));
+
+            triangle = new VBO<Vector3>(new Vector3[] { new Vector3(0, 1, 0), new Vector3(-1, -1, 0), new Vector3(1, -1, 0) });
+            square = new VBO<Vector3>(new Vector3[] { new Vector3(-1, 1, 0), new Vector3(1, 1, 0), new Vector3(1, -1, 0), new Vector3(-1, -1, 0) });
+
+            triangleElements = new VBO<int>(new int[] { 0, 1, 2 }, BufferTarget.ElementArrayBuffer);
+            squareElements = new VBO<int>(new int[] { 0, 1, 2, 3 }, BufferTarget.ElementArrayBuffer);
 
             Glut.glutMainLoop();
         }
@@ -31,6 +45,26 @@ namespace OpenGLTurorial1
             Gl.Viewport(0, 0, width, height);
             Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
+            program.Use();
+
+            // draw my triangle
+            program["model_matrix"].SetValue(Matrix4.CreateTranslation(new Vector3(-1.5f, 0, 0)));
+
+            uint vertexPositionIndex = (uint)Gl.GetAttribLocation(program.ProgramID, "vertexPosition");
+            Gl.EnableVertexAttribArray(vertexPositionIndex);
+            Gl.BindBuffer(triangle);
+            Gl.VertexAttribPointer(vertexPositionIndex, triangle.Size, triangle.PointerType, true, 12, IntPtr.Zero);
+            Gl.BindBuffer(triangleElements);
+
+            Gl.DrawElements(BeginMode.Triangles, triangleElements.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+
+            // draw my square
+            program["model_matrix"].SetValue(Matrix4.CreateTranslation(new Vector3(1.5f, 0, 0)));
+            Gl.BindBufferToShaderAttribute(square, program, "vertexPosition");
+            Gl.BindBuffer(squareElements);
+
+            Gl.DrawElements(BeginMode.Quads, squareElements.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+
             Glut.glutSwapBuffers();
         }
 
@@ -43,7 +77,7 @@ uniform mat4 model_matrix;
 
 void main(void)
 {
-    gl_Position = projection_matrix * view_matrix * vec4(vertexPosition, 1);
+    gl_Position = projection_matrix * view_matrix * model_matrix * vec4(vertexPosition, 1);
 }
 ";
 
