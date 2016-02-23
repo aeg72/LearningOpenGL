b3471f7ad49cef79c78f21eb41321a82a32c0863
aeg72 <aeg72@cornell.edu>
2016-02-23 10:54:27 -0500

    Half-way through Tutorial #2
    Hope this works.

---
+++ OpenGLTurorial1/OpenGLTurorial1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Tao.FreeGlut;
+using OpenGL;
+
+namespace OpenGLTurorial1
+{
+    class Program
+    {
+        private static int width = 1280, height = 720;
+
+        static void Main(string[] args)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutCreateWindow("OpenGL Tutorial");
+
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(OnDisplay);
+
+            Glut.glutMainLoop();
+        }
+
+        private static void OnDisplay()
+        {
+
+        }
+
+        private static void OnRenderFrame()
+        {
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Glut.glutSwapBuffers();
+        }
+
+        public static string VertexShader = @"
+in vec3 vertexPosition;
+
+uniform mat4 projection_matrix;
+uniform mat4 view_matrix;
+uniform mat4 model_matrix;
+
+void main(void)
+{
+    gl_Position = projection_matrix * view_matrix * vec4(vertexPosition, 1);
+}
+";
+
+        public static string FragmentShader = @"
+void main(void)
+{
+    gl_FragColor = vec4(1, 1, 1, 1);
+}
+";
+    }
+}
